63eb87a19add25ff8042e62a0fcdfd6520f4cdb9
Paul Murrell <paul@stat.auckland.ac.nz>
2024-09-10 17:56:53 +1200

    flesh out a lot of Mapping to Visual Features section

--- how-data-vis-works.R
+++ how-data-vis-works.R
@@ -3,6 +3,7 @@
 #| message: false
 library(dplyr)
 library(knitr)
+library(kableExtra)
 library(reshape2)
 library(ggplot2)
 library(scales)
@@ -484,22 +485,25 @@ dev.off()
 
 ## -----------------------------------------------------------------------------
 #| echo: false
+#| message: false
+#| label: tbl-crime-level-total
+#| tbl-colwidths: [20,20]
+#| tbl-cap: A table of the total number of offenders aged 14 to 16 from 2011 to 2021 for different levels of crime.
+crimeLevelTotalTable <- subset(crimeLevelTotal, select=c("level", "total"))
+kable_styling(kable(crimeLevelTotalTable, 
+                    table.attr='data-quarto-disable-processing="true"'),
+              bootstrap_options=c("basic", "hover"), 
+              full_width=FALSE)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
 #| label: fig-bar
-#| fig-cap: A bar plot of number of crimes for different levels of crime. The data symbols in this plot are the bars (highlighted in purple).
+#| fig-cap: A bar plot of number of crimes for different levels of crime. The data symbols in this plot are the horizontal bars.
 ggplot(crimeLevelTotal) + 
-    geom_col(aes(x=total, y=level), fill=highlight) +
+    geom_col(aes(x=total, y=level)) +
     scale_x_continuous(expand=expansion(c(0, .05))) +
-    theme(panel.border=element_rect(colour="grey", fill=NA),
-          plot.title=element_text(colour="grey"),
-          plot.subtitle=element_text(colour="grey"),
-          panel.grid.major.x=element_line(colour="grey80"),
-          panel.grid.minor.x=element_blank(),
-          panel.grid.major.y=element_blank(),
-          panel.grid.minor.y=element_blank(),
-          axis.ticks=element_line(colour="grey"),
-          axis.text=element_text(colour="grey"),
-          axis.title=element_text(colour="grey"),
-          aspect.ratio=1)
+    theme(aspect.ratio=1)
 
 
 ## -----------------------------------------------------------------------------
@@ -515,10 +519,6 @@ ggplot(crimeLevelTotal) +
     theme(panel.border=element_rect(colour="grey", fill=NA),
           plot.title=element_text(colour="grey"),
           plot.subtitle=element_text(colour="grey"),
-          panel.grid.major.x=element_line(colour="grey80"),
-          panel.grid.minor.x=element_blank(),
-          panel.grid.major.y=element_blank(),
-          panel.grid.minor.y=element_blank(),
           axis.ticks=element_line(colour="grey"),
           axis.text=element_text(colour="grey"),
           axis.title=element_text(colour="grey"),
@@ -537,12 +537,351 @@ ggplot(crimeLevelTotal) +
     theme(panel.border=element_rect(colour="grey", fill=NA),
           plot.title=element_text(colour="grey"),
           plot.subtitle=element_text(colour="grey"),
-          panel.grid.major.x=element_line(colour="grey80"),
-          panel.grid.minor.x=element_blank(),
-          panel.grid.major.y=element_blank(),
-          panel.grid.minor.y=element_blank(),
           axis.ticks=element_line(colour="grey"),
           axis.text=element_text(colour="grey"),
           axis.title=element_text(colour="grey"),
           aspect.ratio=1)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-visual-features
+#| fig-cap: Some examples of basic visual features are **position**, **length**, **angle**, **area**, **colour**, and **shape**.
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(6, 2, widths=1:2), gp=gpar(cex=1.5)))
+pushViewport(viewport(layout.pos.row=1, layout.pos.col=2))
+grid.text("position", x=0, just="right")
+grid.segments(.2, .5, .8, .5, gp=gpar(lwd=2))
+grid.segments(.2, .4, .2, .6, gp=gpar(lwd=2))
+grid.segments(.8, .4, .8, .6, gp=gpar(lwd=2))
+grid.circle(c(.3, .6), .7, r=unit(1, "mm"), gp=gpar(fill="black"))
+popViewport()
+pushViewport(viewport(layout.pos.row=2))
+grid.rect(gp=gpar(col=NA, fill="grey90"))
+popViewport()
+pushViewport(viewport(layout.pos.row=2, layout.pos.col=2))
+grid.text("length", x=0, just="right")
+grid.rect(.2, c(.3, .7), c(.1, .4), .2, just="left", gp=gpar(fill="black"))
+popViewport()
+pushViewport(viewport(layout.pos.row=3, layout.pos.col=2))
+grid.text("angle", x=0, just="right")
+grid.lines(c(.55, .45, .55) - .2, c(.6, .4, .4), gp=gpar(lwd=2))
+grid.lines(c(.55, .45, .55) + .2, c(.8, .4, .4), gp=gpar(lwd=2))
+popViewport()
+pushViewport(viewport(layout.pos.row=4))
+grid.rect(gp=gpar(col=NA, fill="grey90"))
+popViewport()
+pushViewport(viewport(layout.pos.row=4, layout.pos.col=2))
+grid.text("area", x=0, just="right")
+grid.circle(c(.3, .7), .5, r=unit(c(1, 3), "mm"), gp=gpar(fill="black"))
+popViewport()
+pushViewport(viewport(layout.pos.row=5, layout.pos.col=2))
+grid.text("colour", x=0, just="right")
+grid.circle(c(.3, .7), .5, r=unit(2, "mm"), gp=gpar(fill=2:3))
+popViewport()
+pushViewport(viewport(layout.pos.row=6))
+grid.rect(gp=gpar(col=NA, fill="grey90"))
+popViewport()
+pushViewport(viewport(layout.pos.row=6, layout.pos.col=2))
+grid.text("shape", x=0, just="right")
+grid.points(c(.3, .7), c(.5, .5), size=unit(4, "mm"), pch=3:4, gp=gpar(lwd=2))
+popViewport()
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-quant-features
+#| fig-height: 3
+#| fig-cap: The visual features position, length, area, and angle are all appropriate visual features for visualising quantitative data values.
+## grid.newpage()
+vp2 <- viewport(width=.9, y=unit(3, "lines"), 
+                height=unit(1, "npc") - unit(6, "lines"), 
+                just="bottom")
+pushViewport(viewport(layout=grid.layout(1, 5)))
+pushViewport(viewport(layout.pos.col=1),
+             vp2)
+## grid.segments(1, 0, 1, 1)
+grid.segments(unit(1, "npc") - unit(2, "mm"), c(0, .5, 1), 
+              unit(1, "npc") + unit(2, "mm"), c(0, .5, 1),
+              arrow=arrow(type="closed", angle=15, length=unit(2, "mm")), 
+              gp=gpar(fill="black"))
+grid.text(c("(zero) 0", "(amount) 10", "(twice) 20"),
+          unit(1, "npc") - unit(4, "mm"), c(0, .5, 1),
+          just="right")
+popViewport(2)
+pushViewport(viewport(layout.pos.col=2),
+             vp2)
+grid.segments(.5, 0, .5, 1, gp=gpar(lty="dotted"))
+grid.points(rep(.5, 3), c(0, .5, 1), pch=16, size=unit(4, "mm"))
+grid.text("position", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=3),
+             vp2)
+grid.rect(1:3/4, 0, .2, c(0, .5, 1), gp=gpar(fill="black"), just="bottom")
+grid.text("length", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=4),
+             vp2)
+grid.circle(.5, c(0, .5, 1), r=unit(sqrt(.5*c(0.001, .5, 1)), "cm"),
+            gp=gpar(fill="black"))
+grid.text("area", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=5),
+             vp2)
+grid.lines(unit(.5, "npc") + unit(c(-4, -4, 4), "mm"),
+           unit(1, "npc") + unit(c(4, -4, -4), "mm"),
+           gp=gpar(lwd=3))
+grid.lines(unit(.5, "npc") + unit(c(2, -4, 4), "mm"),
+           unit(.5, "npc") + unit(c(2, -4, -4), "mm"),
+           gp=gpar(lwd=3))
+grid.segments(unit(.5, "npc") + unit(-4, "mm"), 0,
+              unit(.5, "npc") + unit(4, "mm"), 0,
+              gp=gpar(lwd=3))
+grid.text("angle", y=unit(-1, "lines"))
+popViewport(2)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-not-quant
+#| fig-height: 3
+#| fig-cap: The visual features **colour** and **shape** are not capable of visualising quantitative data values.
+vp2 <- viewport(width=.9, y=unit(3, "lines"), 
+                height=unit(1, "npc") - unit(6, "lines"), 
+                just="bottom")
+pushViewport(viewport(layout=grid.layout(1, 5)))
+pushViewport(viewport(layout.pos.col=1),
+             vp2)
+## grid.segments(1, 0, 1, 1)
+grid.segments(unit(1, "npc") - unit(2, "mm"), c(0, .5, 1), 
+              unit(1, "npc") + unit(2, "mm"), c(0, .5, 1),
+              arrow=arrow(type="closed", angle=15, length=unit(2, "mm")), 
+              gp=gpar(fill="black"))
+grid.text(c("(zero) 0", "(amount) 10", "(twice) 20"),
+          unit(1, "npc") - unit(4, "mm"), c(0, .5, 1),
+          just="right")
+popViewport(2)
+pushViewport(viewport(layout.pos.col=2),
+             vp2)
+cols <- hcl.colors(3, "harmonic")
+grid.points(rep(.5, 3), c(0, .5, 1), pch=21, size=unit(7, "mm"),
+            gp=gpar(col="black", fill=cols))
+grid.text("colour", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=3),
+             vp2)
+grid.points(rep(.5, 3), c(0, .5, 1), pch=2:4, size=unit(5, "mm"),
+            gp=gpar(lwd=2))
+grid.text("shape", y=unit(-1, "lines"))
+popViewport(2)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-qual-features
+#| fig-height: 3
+#| fig-cap: The visual features **colour**, **shape**, and **position** are capable of representing different groups.
+vp2 <- viewport(width=.9, y=unit(3, "lines"), 
+                height=unit(1, "npc") - unit(6, "lines"), 
+                just="bottom")
+pushViewport(viewport(layout=grid.layout(1, 5)))
+pushViewport(viewport(layout.pos.col=1),
+             vp2)
+## grid.segments(1, 0, 1, 1)
+grid.segments(unit(1, "npc") - unit(2, "mm"), c(.2, .5, .8), 
+              unit(1, "npc") + unit(2, "mm"), c(.2, .5, .8),
+              arrow=arrow(type="closed", angle=15, length=unit(2, "mm")), 
+              gp=gpar(fill="black"))
+grid.text(c("Group A", "Group B", "Group C"),
+          unit(1, "npc") - unit(4, "mm"), c(.2, .5, .8),
+          just="right")
+popViewport(2)
+pushViewport(viewport(layout.pos.col=2),
+             vp2)
+cols <- hcl.colors(3, "harmonic")
+grid.points(rep(.5, 3), c(.2, .5, .8), pch=21, size=unit(5, "mm"),
+            gp=gpar(col="black", fill=cols))
+grid.text("colour", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=3),
+             vp2)
+grid.points(rep(.5, 3), c(.2, .5, .8), pch=2:4, size=unit(5, "mm"),
+            gp=gpar(lwd=2))
+grid.text("shape", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=4),
+             vp2)
+grid.rect(rep(.2, 3), c(.2, .5, .8), width=c(.6, .6, .6), height=.1,
+          just="left", gp=gpar(fill="black"))
+grid.text("position", y=unit(-1, "lines"))
+popViewport(2)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-not-qual
+#| fig-height: 3
+#| fig-cap: The visual features **length**, **area**, and **angle** are not appropriate for representing qualitative data values because they imply an ordering of data values that have no inherent order.
+vp2 <- viewport(width=.9, y=unit(3, "lines"), 
+                height=unit(1, "npc") - unit(6, "lines"), 
+                just="bottom")
+pushViewport(viewport(layout=grid.layout(1, 5)))
+pushViewport(viewport(layout.pos.col=1),
+             vp2)
+## grid.segments(1, 0, 1, 1)
+grid.segments(unit(1, "npc") - unit(2, "mm"), c(.2, .5, .8), 
+              unit(1, "npc") + unit(2, "mm"), c(.2, .5, .8),
+              arrow=arrow(type="closed", angle=15, length=unit(2, "mm")), 
+              gp=gpar(fill="black"))
+grid.text(c("Group A", "Group B", "Group C"),
+          unit(1, "npc") - unit(4, "mm"), c(.2, .5, .8),
+          just="right")
+popViewport(2)
+pushViewport(viewport(layout.pos.col=2),
+             vp2)
+grid.rect(1:3/4, 0, .2, c(.2, .5, .8), just="bottom", gp=gpar(fill="black")) 
+grid.text("length", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=3),
+             vp2)
+grid.circle(.5, c(.2, .5, .8), r=unit(sqrt(.5*c(.2, .5, .8)), "cm"),
+            gp=gpar(fill="black"))
+grid.text("area", y=unit(-1, "lines"))
+popViewport(2)
+pushViewport(viewport(layout.pos.col=4),
+             vp2)
+grid.lines(unit(.5, "npc") + unit(c(0, 0), "mm"),
+           unit(.8, "npc") + unit(c(4, -4), "mm"),
+           gp=gpar(lwd=3))
+grid.lines(unit(.5, "npc") + unit(c(-4, 4), "mm"),
+           unit(.5, "npc") + unit(c(-4, 4), "mm"),
+           gp=gpar(lwd=3))
+grid.segments(unit(.5, "npc") + unit(-4, "mm"), .2,
+              unit(.5, "npc") + unit(4, "mm"), .2,
+              gp=gpar(lwd=3))
+grid.text("angle", y=unit(-1, "lines"))
+popViewport(2)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-accuracy
+#| fig-cap: An ordering of basic visual features in terms of their accuracy, with more accurate at the top.
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(6, 2, widths=1:2), gp=gpar(cex=1.5)))
+pushViewport(viewport(layout.pos.row=1, layout.pos.col=2))
+grid.text("position", x=0, just="right")
+grid.segments(.2, .5, .8, .5, gp=gpar(lwd=2))
+grid.segments(.2, .4, .2, .6, gp=gpar(lwd=2))
+grid.segments(.8, .4, .8, .6, gp=gpar(lwd=2))
+grid.circle(c(.3, .6), .7, r=unit(1, "mm"), gp=gpar(fill="black"))
+popViewport()
+pushViewport(viewport(layout.pos.row=2))
+grid.rect(gp=gpar(col=NA, fill="grey90"))
+popViewport()
+pushViewport(viewport(layout.pos.row=2, layout.pos.col=2))
+grid.text("length", x=0, just="right")
+grid.rect(.2, c(.3, .7), c(.1, .4), .2, just="left", gp=gpar(fill="black"))
+popViewport()
+pushViewport(viewport(layout.pos.row=3, layout.pos.col=2))
+grid.text("angle", x=0, just="right")
+grid.lines(c(.55, .45, .55) - .2, c(.6, .4, .4), gp=gpar(lwd=2))
+grid.lines(c(.55, .45, .55) + .2, c(.8, .4, .4), gp=gpar(lwd=2))
+popViewport()
+pushViewport(viewport(layout.pos.row=4))
+grid.rect(gp=gpar(col=NA, fill="grey90"))
+popViewport()
+pushViewport(viewport(layout.pos.row=4, layout.pos.col=2))
+grid.text("area", x=0, just="right")
+grid.circle(c(.3, .7), .5, r=unit(c(1, 3), "mm"), gp=gpar(fill="black"))
+popViewport()
+pushViewport(viewport(layout.pos.row=5, layout.pos.col=2))
+grid.text("colour", x=0, just="right")
+grid.circle(c(.3, .7), .5, r=unit(2, "mm"), 
+            gp=gpar(fill=hcl(240, c(40, 70), c(40, 70))))
+popViewport()
+pushViewport(viewport(layout.pos.row=6))
+grid.rect(gp=gpar(col=NA, fill="grey90"))
+popViewport()
+pushViewport(viewport(layout.pos.row=6, layout.pos.col=2))
+grid.text("shape", x=0, just="right")
+grid.points(c(.3, .7), c(.5, .5), size=unit(4, "mm"), 
+            pch=c(4, 8), gp=gpar(lwd=2))
+popViewport()
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-colour-cap
+#| fig-height: 3
+#| fig-cap: Mapping qualitative data values to colour works well for a small number of colours, but less well for a large number of colours.
+pie <- function(n) {
+    t <- rev(seq(0, 2*pi, length.out=100*n) + pi/2)
+    cols <- scales::hue_pal()(n)
+    for (i in 1:n) {
+        index <- 1:100 + (i-1)*100
+        x <- cos(t[index])
+        y <- sin(t[index])
+        grid.polygon(c(.5, .5 + .3*x), c(.5, .5 + .3*y), 
+                     gp=gpar(col=figbg, lwd=4, fill=cols[i]))
+        grid.text(letters[i], .5 + mean(.25*x), .5 + mean(.25*y),
+                  gp=gpar(cex=1))
+    }
+}
+pushViewport(viewport(layout=grid.layout(1, 3, respect=TRUE)))
+pushViewport(viewport(layout.pos.col=1))
+pie(3)
+popViewport()
+pushViewport(viewport(layout.pos.col=2))
+pie(7)
+popViewport()
+pushViewport(viewport(layout.pos.col=3))
+pie(11)
+popViewport()
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-pos-cap
+#| fig-height: 3
+#| fig-cap: Mapping qualitative data values to position works well for both a small number of positions and for a large number of positions.
+bars <- function(n) {
+    h <- 3
+    y <- unit(.5, "npc") + unit((h + 3)*(seq(1:n) - n/2), "mm")
+    grid.rect(.5, y, .3, unit(h, "mm"), gp=gpar(fill="grey"))
+    grid.text(rev(letters[1:n]), unit(.35, "npc") - unit(.5, "lines"), y)
+}
+pushViewport(viewport(layout=grid.layout(1, 3, respect=TRUE)))
+pushViewport(viewport(layout.pos.col=1))
+bars(3)
+popViewport()
+pushViewport(viewport(layout.pos.col=2))
+bars(7)
+popViewport()
+pushViewport(viewport(layout.pos.col=3))
+bars(11)
+popViewport()
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-dot
+#| fig-cap: A dot plot of the number of crimes for different levels of crime. The data symbols in this plot are the data points.
+ggplot(crimeLevelTotal) + 
+    geom_segment(aes(x=-Inf, xend=Inf, y=level, yend=level), 
+                 linetype="dotted") +
+    geom_point(aes(x=total, y=level)) +
+    theme(aspect.ratio=1)
+
+
+## -----------------------------------------------------------------------------
+#| echo: false
+#| label: fig-pie
+#| fig-cap: A pie chart of the number of crimes for different levels of crime. The data symbols in this plot are the pie wedges.
+ggplot(crimeLevelTotal) + 
+    geom_col(aes(x=total, y="", fill=level), colour="black") +
+    coord_polar() +
+    theme(aspect.ratio=1,
+          axis.title.y=element_blank(),
+          axis.ticks.y=element_blank())
 
