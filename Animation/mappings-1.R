b2f199f8efbe541f8c7650bce1fcd651923082ec
Paul Murrell <paul@stat.auckland.ac.nz>
2024-09-12 17:22:16 +1200

    improved animations?

--- Animation/mappings-1.R
+++ Animation/mappings-1.R
@@ -96,15 +96,27 @@ popViewport()
 grid.force()
 grid.add("key-5-1-1.7-2-7-2", grep=TRUE,
          textGrob("", unit(-.5, "lines"), name="age.end"))
+yearCopy <- editGrob(grid.get("core-fg.4-1-4-1::text", grep=TRUE),
+                     gp=gpar(col="grey", fontface="plain"), name="year.copy")
+grid.add("core-fg.4-1-4-1", grep=TRUE, yearCopy)
+grid.reorder("core-fg.4-1-4-1", "year.copy")
+rateCopy <- editGrob(grid.get("core-fg.4-2-4-2::text", grep=TRUE),
+                     gp=gpar(col="grey", fontface="plain"), name="rate.copy")
+grid.add("core-fg.4-2-4-2", grep=TRUE, rateCopy)
+grid.reorder("core-fg.4-2-4-2", "rate.copy")
+ageCopy <- editGrob(grid.get("core-fg.4-3-4-3::text", grep=TRUE),
+                     gp=gpar(col="grey", fontface="plain"), name="age.copy")
+grid.add("core-fg.4-3-4-3", grep=TRUE, ageCopy)
+grid.reorder("core-fg.4-3-4-3", "age.copy")
 
 dev.off()
 
 library(animaker)
 
-moveYear <- atomic(dur=2, label="moveYear")
+moveYear <- atomic(dur=1.5, label="moveYear")
 showYear <- atomic(dur=0, label="showYear")
 year <- vec(moveYear, showYear, label="year")
-moveRate <- atomic(dur=2, label="moveRate")
+moveRate <- atomic(dur=1, label="moveRate")
 showRate <- atomic(dur=0, label="showRate")
 rate <- vec(moveRate, showRate, label="rate")
 moveAge <- atomic(dur=2, label="moveAge")
@@ -120,20 +132,26 @@ library(xml2)
 svg <- read_xml("mappings-1.svg")
 ns <- xml_ns_rename(xml_ns(svg), d1="svg")
 yearStartGroup <-
-    xml_find_first(svg, '//svg:g[@id = "core-fg.4-1-4-1.1"]/svg:g/svg:g', ns)
+    xml_find_first(svg, '//svg:g[@id = "core-fg.4-1-4-1.1"]/svg:g[2]/svg:g', ns)
 yearEndGroup <- xml_find_first(svg, '//svg:g[@id = "year.end.1.1"]', ns)
 yearLine <- xml_find_first(svg, '//svg:polyline[@id = "year.line.1.1"]', ns)
 xml_set_attr(yearStartGroup, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
 xml_set_attr(yearLine, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
+yearStart <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                      xml_attr(yearStartGroup, "transform")),
+                                 " ")[[1]])
+yearEnd <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                    xml_attr(yearEndGroup, "transform")),
+                               " ")[[1]])
+yearDiff <- yearEnd - yearStart
 xml_add_child(yearStartGroup,
-              "animateTransform",
-              attributeName="transform",
-              attributeType="XML",
-              type="translate",
-              from=gsub("[^0-9. ]", "", xml_attr(yearStartGroup, "transform")),
-              to=gsub("[^0-9. ]", "", xml_attr(yearEndGroup, "transform")),
+              "animateMotion",
+              path=paste0("M0,0",
+                          " C0,", yearDiff[2]/2,
+                          " ", yearDiff[1]/2, ",", yearDiff[2],
+                          " ", yearDiff[1], ",", yearDiff[2]),
               dur=paste0(timings$moveYear$durn, "s"),
               fill="freeze",
               restart="always")
@@ -145,20 +163,26 @@ xml_add_child(yearLine,
               dur="indefinite",
               restart="always")              
 rateStartGroup <-
-    xml_find_first(svg, '//svg:g[@id = "core-fg.4-2-4-2.1"]/svg:g/svg:g', ns)
+    xml_find_first(svg, '//svg:g[@id = "core-fg.4-2-4-2.1"]/svg:g[2]/svg:g', ns)
 rateEndGroup <- xml_find_first(svg, '//svg:g[@id = "rate.end.1.1"]', ns)
 rateLine <- xml_find_first(svg, '//svg:polyline[@id = "rate.line.1.1"]', ns)
 xml_set_attr(rateStartGroup, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
 xml_set_attr(rateLine, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
+rateStart <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                      xml_attr(rateStartGroup, "transform")),
+                                 " ")[[1]])
+rateEnd <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                    xml_attr(rateEndGroup, "transform")),
+                               " ")[[1]])
+rateDiff <- rateEnd - rateStart
 xml_add_child(rateStartGroup,
-              "animateTransform",
-              attributeName="transform",
-              attributeType="XML",
-              type="translate",
-              from=gsub("[^0-9. ]", "", xml_attr(rateStartGroup, "transform")),
-              to=gsub("[^0-9. ]", "", xml_attr(rateEndGroup, "transform")),
+              "animateMotion",
+              path=paste0("M0,0",
+                          " C0,", rateDiff[2]/2,
+                          " ", rateDiff[1]/2, ",", rateDiff[2],
+                          " ", rateDiff[1], ",", rateDiff[2]),
               begin=paste0(timings$moveRate$start, "s"),
               dur=paste0(timings$moveRate$durn, "s"),
               fill="freeze",
@@ -171,7 +195,7 @@ xml_add_child(rateLine,
               dur="indefinite",
               restart="always")              
 ageStartGroup <-
-    xml_find_first(svg, '//svg:g[@id = "core-fg.4-3-4-3.1"]/svg:g/svg:g', ns)
+    xml_find_first(svg, '//svg:g[@id = "core-fg.4-3-4-3.1"]/svg:g[2]/svg:g', ns)
 ageEndGroup <- xml_find_first(svg, '//svg:g[@id = "age.end.1.1"]', ns)
 ageLine <-
     xml_find_first(svg,
@@ -179,13 +203,19 @@ ageLine <-
                    ns)
 xml_set_attr(ageStartGroup, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
+ageStart <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                      xml_attr(ageStartGroup, "transform")),
+                                 " ")[[1]])
+ageEnd <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                    xml_attr(ageEndGroup, "transform")),
+                               " ")[[1]])
+ageDiff <- ageEnd - ageStart
 xml_add_child(ageStartGroup,
-              "animateTransform",
-              attributeName="transform",
-              attributeType="XML",
-              type="translate",
-              from=gsub("[^0-9. ]", "", xml_attr(ageStartGroup, "transform")),
-              to=gsub("[^0-9. ]", "", xml_attr(ageEndGroup, "transform")),
+              "animateMotion",
+              path=paste0("M0,0",
+                          " C", ageDiff[1]/2, ",0", 
+                          " ", ageDiff[1]/2, ",0",
+                          " ", ageDiff[1], ",", ageDiff[2]),
               begin=paste0(timings$moveAge$start, "s"),
               dur=paste0(timings$moveAge$durn, "s"),
               fill="freeze",
--- Animation/mappings-2.R
+++ Animation/mappings-2.R
@@ -100,18 +100,30 @@ grid.add("bar.3-2-3-2", grep=TRUE,
                       name="rate.line"))
 grid.add("bar.3-2-3-2", grep=TRUE,
          textGrob("", x=unit(-1, "lines"), y=1, name="rate.end"))
+yearCopy <- editGrob(grid.get("core-fg.4-1-4-1::text", grep=TRUE),
+                     gp=gpar(col="grey", fontface="plain"), name="year.copy")
+grid.add("core-fg.4-1-4-1", grep=TRUE, yearCopy)
+grid.reorder("core-fg.4-1-4-1", "year.copy")
+rateCopy <- editGrob(grid.get("core-fg.4-2-4-2::text", grep=TRUE),
+                     gp=gpar(col="grey", fontface="plain"), name="rate.copy")
+grid.add("core-fg.4-2-4-2", grep=TRUE, rateCopy)
+grid.reorder("core-fg.4-2-4-2", "rate.copy")
+ageCopy <- editGrob(grid.get("core-fg.4-3-4-3::text", grep=TRUE),
+                     gp=gpar(col="grey", fontface="plain"), name="age.copy")
+grid.add("core-fg.4-3-4-3", grep=TRUE, ageCopy)
+grid.reorder("core-fg.4-3-4-3", "age.copy")
 
 dev.off()
 
 library(animaker)
 
-moveYear <- atomic(dur=2, label="moveYear")
+moveYear <- atomic(dur=1.5, label="moveYear")
 showYear <- atomic(dur=0, label="showYear")
 year <- vec(moveYear, showYear, label="year")
 moveRate <- atomic(dur=2, label="moveRate")
 showRate <- atomic(dur=0, label="showRate")
 rate <- vec(moveRate, showRate, label="rate")
-moveAge <- atomic(dur=2, label="moveAge")
+moveAge <- atomic(dur=1, label="moveAge")
 showAge <- atomic(dur=0, label="showAge")
 age <- vec(moveAge, showAge, label="age")
 anim <- vec(year, age, rate, durn=20, label="anim")
@@ -124,20 +136,26 @@ library(xml2)
 svg <- read_xml("mappings-2.svg")
 ns <- xml_ns_rename(xml_ns(svg), d1="svg")
 yearStartGroup <-
-    xml_find_first(svg, '//svg:g[@id = "core-fg.4-1-4-1.1"]/svg:g/svg:g', ns)
+    xml_find_first(svg, '//svg:g[@id = "core-fg.4-1-4-1.1"]/svg:g[2]/svg:g', ns)
 yearEndGroup <- xml_find_first(svg, '//svg:g[@id = "year.end.1.1"]', ns)
 yearLine <- xml_find_first(svg, '//svg:polyline[@id = "year.line.1.1"]', ns)
 xml_set_attr(yearStartGroup, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
 xml_set_attr(yearLine, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
+yearStart <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                      xml_attr(yearStartGroup, "transform")),
+                                 " ")[[1]])
+yearEnd <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                    xml_attr(yearEndGroup, "transform")),
+                               " ")[[1]])
+yearDiff <- yearEnd - yearStart
 xml_add_child(yearStartGroup,
-              "animateTransform",
-              attributeName="transform",
-              attributeType="XML",
-              type="translate",
-              from=gsub("[^0-9. ]", "", xml_attr(yearStartGroup, "transform")),
-              to=gsub("[^0-9. ]", "", xml_attr(yearEndGroup, "transform")),
+              "animateMotion",
+              path=paste0("M0,0",
+                          " C0,", yearDiff[2]/2,
+                          " ", yearDiff[1]/2, ",", yearDiff[2],
+                          " ", yearDiff[1], ",", yearDiff[2]),
               dur=paste0(timings$moveYear$durn, "s"),
               fill="freeze",
               restart="always")
@@ -149,20 +167,26 @@ xml_add_child(yearLine,
               dur="indefinite",
               restart="always")              
 ageStartGroup <-
-    xml_find_first(svg, '//svg:g[@id = "core-fg.4-3-4-3.1"]/svg:g/svg:g', ns)
+    xml_find_first(svg, '//svg:g[@id = "core-fg.4-3-4-3.1"]/svg:g[2]/svg:g', ns)
 ageEndGroup <- xml_find_first(svg, '//svg:g[@id = "age.end.1.1"]', ns)
 ageLine <- xml_find_first(svg, '//svg:polyline[@id = "age.line.1.1"]', ns)
 xml_set_attr(ageStartGroup, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
 xml_set_attr(ageLine, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
+ageStart <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                      xml_attr(ageStartGroup, "transform")),
+                                 " ")[[1]])
+ageEnd <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                    xml_attr(ageEndGroup, "transform")),
+                               " ")[[1]])
+ageDiff <- ageEnd - ageStart
 xml_add_child(ageStartGroup,
-              "animateTransform",
-              attributeName="transform",
-              attributeType="XML",
-              type="translate",
-              from=gsub("[^0-9. ]", "", xml_attr(ageStartGroup, "transform")),
-              to=gsub("[^0-9. ]", "", xml_attr(ageEndGroup, "transform")),
+              "animateMotion",
+              path=paste0("M0,0",
+                          " C0,", ageDiff[2]/2,
+                          " ", ageDiff[1]/2, ",", ageDiff[2],
+                          " ", ageDiff[1], ",", ageDiff[2]),
               begin=paste0(timings$moveAge$start, "s"),
               dur=paste0(timings$moveAge$durn, "s"),
               fill="freeze",
@@ -175,18 +199,24 @@ xml_add_child(ageLine,
               dur="indefinite",
               restart="always")              
 rateStartGroup <-
-    xml_find_first(svg, '//svg:g[@id = "core-fg.4-2-4-2.1"]/svg:g/svg:g', ns)
+    xml_find_first(svg, '//svg:g[@id = "core-fg.4-2-4-2.1"]/svg:g[2]/svg:g', ns)
 rateEndGroup <- xml_find_first(svg, '//svg:g[@id = "rate.end.1.1"]', ns)
 rateLine <- xml_find_first(svg, '//svg:polyline[@id = "rate.line.1.1"]', ns)
 xml_set_attr(rateStartGroup, "style",
              "filter: drop-shadow(3px -3px 2px rgba(0,0,0,.4)")
+rateStart <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                      xml_attr(rateStartGroup, "transform")),
+                                 " ")[[1]])
+rateEnd <- as.numeric(strsplit(gsub("[^0-9. ]", "",
+                                    xml_attr(rateEndGroup, "transform")),
+                               " ")[[1]])
+rateDiff <- rateEnd - rateStart
 xml_add_child(rateStartGroup,
-              "animateTransform",
-              attributeName="transform",
-              attributeType="XML",
-              type="translate",
-              from=gsub("[^0-9. ]", "", xml_attr(rateStartGroup, "transform")),
-              to=gsub("[^0-9. ]", "", xml_attr(rateEndGroup, "transform")),
+              "animateMotion",
+              path=paste0("M0,0",
+                          " C0,", rateDiff[2],
+                          " ", rateDiff[1]/2, ",", rateDiff[2],
+                          " ", rateDiff[1], ",", rateDiff[2]),
               begin=paste0(timings$moveRate$start, "s"),
               dur=paste0(timings$moveRate$durn, "s"),
               fill="freeze",
