a094e4d2369233746e07e1be5259cd57439c94ea
Paul Murrell <paul@stat.auckland.ac.nz>
2025-04-01 12:51:54 +1300

    got single stimulus working

--- Memory/memory.R
+++ Memory/memory.R
@@ -4,31 +4,82 @@ library(grid)
 
 luminance <- 60
 chroma <- 60
-hues <- runif(10, 0, 360)
+hues <- floor(runif(10, 1, 360))
 
 cols <- hcl(hues, chroma, luminance)
 
-t <- seq(0, 2*pi, length.out=360)
+t <- seq(0, 2*pi, length.out=361)[-361]
 x <- unit(.5, "npc") + unit(cos(t), "in")
 y <- unit(.5, "npc") + unit(sin(t), "in")
 
-grid.newpage()
-## Stimuli
-grid.circle(r=unit(1, "cm"), gp=gpar(col=cols, fill=cols),
-            name="stimulus")
+pdf(width=3, height=3)
 
-## gridSVG (luminance) mask
-pushMask(mask(circleGrob(r=unit(1, "in"), gp=gpar(lwd=40, col="white"))))
+grid.newpage()
+
+## Stimuli
+elementName <- function(stem, col) {
+    paste0(stem, col)
+}
+for (i in seq_along(cols)) {
+    grid.circle(r=unit(1, "cm"), gp=gpar(col=cols[i], fill=cols[i]),
+                name=elementName("stimulus-", hues[i]))
+}
+
+## Match
+## Draw segments from centre ...
 grid.segments(.5, .5, x, y,
               gp=gpar(col=hcl(t/pi*180, chroma, luminance), lwd=3),
               name="match")
-popMask()
+## ... but obscure centre with circle
+## (which also will intercept click events (when visible)
+grid.circle(r=unit(1, "in") - unit(5, "mm"),
+            gp=gpar(col="white", fill="white"),
+            name="mask")
+## Circles to show match
+for (i in seq_along(x)) {
+    grid.circle(x[i], y[i], r=unit(1, "mm"),
+                gp=gpar(fill=NA),
+                name=elementName("matchMark-", i))
+}
+## Circles to show stimuli
+for (i in seq_along(cols)) {
+    grid.circle(x[hues[i]], y[hues[i]], r=unit(1, "mm"),
+                gp=gpar(fill="black"),
+                name=elementName("stimMark-", hues[i]))
+}
 
-grid.garnish("stimulus", onclick="phaseTwo(this)")
-grid.garnish("stimulus", class="stimulus")
+## Result message
+grid.text("Error = 00.0", y=.5, name="error")
 
-grid.garnish("match", onclick=rep("phaseOne(this)", 360), group=FALSE)
+## Play again
+paText <- textGrob("play again", gp=gpar(col="white"), name="play-again-text")
+paRect <- roundrectGrob(width=grobWidth(paText) + unit(4, "mm"),
+                        height=grobHeight(paText) + unit(4, "mm"),
+                        gp=gpar(col="#333", fill="#333"))
+paTree <- grobTree(paRect, paText, 
+                   vp=viewport(y=unit(1, "lines")),
+                   name="play-again")
+grid.draw(paTree)
+
+## Starting visibility and onclicks
+for (i in seq_along(cols)) {
+    grid.garnish(elementName("stimulus-", hues[i]),
+                 onclick="state.phaseTwo(this)", class="stimulus")
+}
+grid.garnish("match", onclick=rep("state.phaseThree(this)", 360), group=FALSE)
 grid.garnish("match", visibility="hidden")
+grid.garnish("mask", visibility="hidden")
+for (i in seq_along(x)) {
+    grid.garnish(elementName("matchMark-", i),
+                 visibility="hidden", class="matchMark")
+}
+for (i in seq_along(cols)) {
+    grid.garnish(elementName("stimMark-", hues[i]),
+                 visibility="hidden", class="stimMark")
+}
+grid.garnish("error", visibility="hidden")
+grid.garnish("play-again", visibility="hidden", onclick="state.phaseOne()")
+grid.garnish("play-again-text", "pointer-events"="none")
 
 ## For debugging
 grid.export("memory.svg")
@@ -41,4 +92,3 @@ writeLines(c(readLines("memory.svg"),
 ## Just SVG file
 grid.script(paste(readLines("memory.js"), collapse="\n"))
 grid.export("memory.svg")
-
--- Memory/memory.js
+++ Memory/memory.js
@@ -1,29 +1,79 @@
 
- 
-function phaseOne(x) {
-    const stimuli = document.getElementsByClassName("stimulus");
-    for (let i = 0; i < stimuli.length; i++) {
-        stimuli[i].style.visibility = "visible";
-        stimuli[i].style.pointerEvents = "all";
-    }
-    const match = document.getElementById("match.1");
-    const matches = match.children;
-    for (let i = 0; i < matches.length; i++) {
-        matches[i].style.visibility = "hidden";
-        matches[i].setAttribute("pointer-events", "none");
-    }
-}
+const state = (function() {
+    let stimCol = 0;
+    let matchCol = 0;
 
-function phaseTwo(x) {
-    const stimuli = document.getElementsByClassName("stimulus");
-    for (let i = 0; i < stimuli.length; i++) {
-        stimuli[i].style.visibility = "hidden";
-        stimuli[i].style.pointerEvents = "none";
+    function angleDiff(x, y) {
+        var diff = Math.abs(x - y);
+        if (diff > 180)
+            diff = 360 - diff;
+        return diff;
     }
-    const match = document.getElementById("match.1");
-    const matches = match.children;
-    for (let i = 0; i < matches.length; i++) {
-        matches[i].style.visibility = "visible";
-        matches[i].setAttribute("pointer-events", "stroke");
+
+    function visID(id, visibility) {
+        const elt = document.getElementById(id);
+        elt.style.visibility = visibility;
     }
-}
+    
+    function showID(id) { visID(id, "visible"); }
+    function hideID(id) { visID(id, "hidden"); }
+
+    function visClass(cl, visibility) {
+        const elts = document.getElementsByClassName(cl);
+        for (let i = 0; i < elts.length; i++) {
+            elts[i].style.visibility = visibility;
+        }
+    }
+
+    function showClass(id) { visClass(id, "visible"); }
+    function hideClass(id) { visClass(id, "hidden"); }
+
+    return {
+
+        phaseOne(x) {
+            /* Initialisation */
+            stimCol = 0;
+            matchCol = 0;
+            /* Show random stimulus */
+            const stimuli = document.getElementsByClassName("stimulus");
+            const n = stimuli.length;
+            const which = Math.floor(Math.random()*n + 1) + 1;
+            showID(stimuli[which].id);
+            /* Hide match */
+            hideID("mask.1");
+            hideID("match.1");
+            hideClass("matchMark");
+            hideClass("stimMark");
+            hideID("error.1");
+            hideID("play-again.1");
+        },
+        
+        phaseTwo(x) {
+            stimCol = parseInt(x.id.replaceAll(/.+-|[.].+/g, ""));
+            /* Hide stimuli */
+            hideClass("stimulus");
+            /* Show match */
+            showID("mask.1");
+            showID("match.1");
+        },
+        
+        phaseThree(x) {
+            matchCol = parseInt(x.id.replace(/.+[.]/, ""));
+            error = angleDiff(matchCol, stimCol);
+            /* Show stimulus mark */
+            showID("stimMark-" + stimCol + ".1");
+            /* Show match mark */
+            showID("matchMark-" + matchCol + ".1");
+            /* Show error */
+            errorMsg = document.getElementById("error.1.1.tspan.1");
+            errorMsg.textContent = "Error = " + error.toFixed(1);
+            showID("error.1");
+            /* Show play again */
+            showID("play-again.1");
+        }
+    };
+})();
+
+
+
+
