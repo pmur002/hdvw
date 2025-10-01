a7936f362775ef208c1bf52315b9299ef54afdac
Paul Murrell <paul@stat.auckland.ac.nz>
2025-10-02 12:37:19 +1300

    working version of optimal colour solid

--- HCL/cie-spectral-solid-boundary.R
+++ HCL/cie-spectral-solid-boundary.R
@@ -1,6 +1,73 @@
 
-library(rgl)
+xyz <- read.csv("cie_xyz_spectral_boundary.csv")
 
-z <- read.csv("cie_luv_spectral_boundary_google.csv")
+viewXYZ <- function() {
+    library(rgl)
+    plot3d(xyz)   
+    points3d(0, 0, 0, col="red", size=10)
+    points3d(xyzMax, col="red", size=10)
+    points3d(seq(0, xyzMax[1], length.out=50),
+             seq(0, xyzMax[2], length.out=50),
+             seq(0, xyzMax[3], length.out=50),
+             col="red")            
+}
 
-plot3d(z)
+xyzMax <- c(xyz[,1][which.max(xyz[,1])],
+            xyz[,2][which.max(xyz[,2])],
+            xyz[,3][which.max(xyz[,3])])
+lineOfGreys <- cbind(seq(0, xyzMax[1], length.out=50),
+                     seq(0, xyzMax[2], length.out=50),
+                     seq(0, xyzMax[3], length.out=50))
+
+library(colorspace)
+luv <- coords(as(XYZ(xyz[,1], xyz[,2], xyz[,3]), "LUV"))
+
+viewLUV <- function() {
+    library(rgl)
+    plot3d(luv)
+    points3d(coords(as(XYZ(lineOfGreys[,1],
+                           lineOfGreys[,2],
+                           lineOfGreys[,3]), "LUV")),
+             col="red")
+}
+
+library(gMOIP)
+hull <- convexHull(as.matrix(luv[!duplicated(luv), ]))
+plotHull3D(as.matrix(luv[!duplicated(luv), ]))
+
+## plotPoints3D(c(50, 0, 0), addText=TRUE)
+inHull(coords(as(polarLUV(50, 0, 0), "LUV")), luv[!duplicated(luv), ])
+inHull(coords(as(polarLUV(50, 0:10, 0), "LUV")), luv[!duplicated(luv), ])
+inHull(coords(as(polarLUV(0, 50, 0), "LUV")), luv[!duplicated(luv), ])
+
+## Generate convex hull once
+convhull <- geometry::convhulln(luv[!duplicated(luv), ])
+inHull(coords(as(polarLUV(0, 50, 0), "LUV")), luv[!duplicated(luv), ],
+       hull=convhull)
+
+notrun <- function() {
+    z <- read.csv("cie_luv_spectral_boundary.csv")
+
+    library(rgl)
+    plot3d(z)
+
+    library(gMOIP)
+    hull <- convexHull(as.matrix(z[!duplicated(z), ]))
+    plotHull3D(as.matrix(z[!duplicated(z), ]))
+}
+
+###############################################################################
+
+doesnotrun <- function() {
+    library(cxhull)
+
+    ## Just stops with message "Killed" !?
+    hull <- cxhull(as.matrix(z[!duplicated(z), ]), triangulate=TRUE)
+
+    plotConvexHull3d(hull)
+
+    ## Fails to even install
+    library(cgalMeshes)
+}
+
+
--- HCL/hcl.R
+++ HCL/hcl.R
@@ -3,11 +3,12 @@
 
 library(grid)
 library(colorspace)
+library(gMOIP)
 
-u <- -180:180
-v <- -180:180
+u <- -200:200
+v <- -200:200
 L <- 100:0
-C <- 0:180
+C <- 0:200
 
 r <- .475
 d <- .5 - r
@@ -19,25 +20,65 @@ sintheta <- sin(angle/180*pi)
 
 dotalpha <- .5
 
-colorFromUV <- function(v, u, L=50) {
-    srgb <- coords(as(LUV(L, u, v), "sRGB"))
-    unreal <- apply(srgb, 1, function(row) any(row < 0 | row > 1))
-    cols <- character(nrow(srgb))
-    if (any(!unreal))
-        cols[!unreal] <- rgb(srgb[!unreal, , drop=FALSE])
-    if (any(unreal))
-        cols[unreal] <- NA
+## Optimal colour solid?
+z <- read.csv("cie_xyz_spectral_boundary.csv")
+library(colorspace)
+luvSolid <- coords(as(XYZ(z[,1], z[,2], z[,3]), "LUV"))
+luvHull <- geometry::convhulln(luvSolid[!duplicated(luvSolid), ])
+
+colorFromUV <- function(v, u, L=50, sRGB=TRUE) {
+    if (sRGB) {
+        srgb <- coords(as(LUV(L, u, v), "sRGB"))
+        unreal <- apply(srgb, 1, function(row) any(row < 0 | row > 1))
+        cols <- character(nrow(srgb))
+        if (any(!unreal))
+            cols[!unreal] <- rgb(srgb[!unreal, , drop=FALSE])
+        if (any(unreal))
+            cols[unreal] <- NA
+    } else {
+        luv <- LUV(L, u, v)
+        lch <- coords(as(luv, "polarLUV"))
+        rgb <- hcl(lch[,3], lch[,2], lch[,1], fixup=TRUE)
+        ## Cannot call inHull() on too many points at once
+        real <- logical(length(rgb))
+        start <- 1
+        repeat {
+            end <- min(length(rgb), start + 9999)
+            real[start:end] <-
+                inHull(coords(luv)[start:end, ],
+                       luvSolid[!duplicated(luvSolid), ],
+                       luvHull) >= 0
+            if (end == length(rgb))
+                break
+            start <- end + 1
+        }
+        if (any(!real)) {
+            rgb[!real] <- NA
+        }
+        cols <- rgb   
+    }
     cols
 }
 
-colorFromHue <- function(L, C, H=0) {
-    srgb <- coords(as(polarLUV(L, C, H), "sRGB"))
-    unreal <- apply(srgb, 1, function(row) any(row < 0 | row > 1))
-    cols <- character(nrow(srgb))
-    if (any(!unreal))
-        cols[!unreal] <- rgb(srgb[!unreal, , drop=FALSE])
-    if (any(unreal))
-        cols[unreal] <- NA
+colorFromHue <- function(L, C, H=0, sRGB=TRUE) {
+    if (sRGB) {
+        srgb <- coords(as(polarLUV(L, C, H), "sRGB"))
+        unreal <- apply(srgb, 1, function(row) any(row < 0 | row > 1))
+        cols <- character(nrow(srgb))
+        if (any(!unreal))
+            cols[!unreal] <- rgb(srgb[!unreal, , drop=FALSE])
+        if (any(unreal))
+            cols[unreal] <- NA
+    } else {
+        rgb <- hcl(H, C, L, fixup=TRUE)
+        real <- inHull(coords(as(polarLUV(L, C, H), "LUV")),
+                       luvSolid[!duplicated(luvSolid), ],
+                       luvHull) >= 0
+        if (any(!real)) {
+            rgb[!real] <- NA
+        }
+        cols <- rgb   
+    }
     cols
 }
 
@@ -82,15 +123,25 @@ slice <- function(cols, up=TRUE) {
     } else {
         dy <- -1
     }
+    circles <- circleGrob(c(.5 - r*costheta, .5 + r*costheta),
+                          c(.5 + dy*r*sintheta, .5 + dy*r*sintheta),
+                          unit(1, "mm"),
+                          gp=gpar(col=NA, fill=rgb(0, 0, 0)))
+    circleMask <- fillGrob(grobTree(rectGrob(width=2),
+                                    circles),
+                           gp=gpar(fill="black"))
+    grid.draw(editGrob(circles, gp=gpar(fill=rgb(1, 1, 1, dotalpha))))
+    pushViewport(viewport(mask=circleMask))
     grid.line.to(.5 - r*costheta, .5 + dy*r*sintheta,
                  gp=gpar(col=rgb(1, 1, 1, dotalpha), lty="dotted"))
     grid.segments(.5, .5, .5 - r*costheta, .5 + dy*r*sintheta,
                   gp=gpar(col=rgb(1, 1, 1, dotalpha), lty="dotted"))
     grid.segments(.5, .5, .5 + r*costheta, .5 + dy*r*sintheta,
                   gp=gpar(col=rgb(1, 1, 1, dotalpha), lty="dotted"))
+    popViewport()
     grid.draw(ras)
-    grid.circle(.5, .5, unit(.5, "mm"),
-                gp=gpar(col=rgb(1, 1, 1, .2)))
+    grid.circle(.5, .5, unit(1, "mm"),
+                gp=gpar(col=NA, fill=rgb(1, 1, 1, dotalpha)))
     pushViewport(viewport(mask=ras))
     grid.segments(.5, .5, .5  - r*costheta, .5 + dy*r*sintheta,
                   gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
@@ -100,115 +151,124 @@ slice <- function(cols, up=TRUE) {
     grid.move.to(.5 + r*costheta, .5 + dy*r*sintheta)
 }
 
-grid.newpage()
-grid.rect(gp=gpar(fill="black"))
-pushViewport(viewport(width=.8, height=.8,
-                      layout=grid.layout(5, 5,
-                                         widths=c(1, .5, 2, .5, 1),
-                                         heights=c(2, .5, 2, .5, 2),
-                                         respect=TRUE)))
+hclDiagram <- function(sRGB=TRUE) {
+    grid.newpage()
+    grid.rect(gp=gpar(fill="black"))
+    pushViewport(viewport(width=.9, height=.9,
+                          layout=grid.layout(5, 5,
+                                             widths=c(1, .5, 2, .5, 1),
+                                             heights=c(2, .5, 2, .5, 2),
+                                             respect=TRUE),
+                          gp=gpar(fill=NA, lwd=2)))
 
-pushViewport(viewport(layout.pos.col=1:5, layout.pos.row=1:5))
-popViewport()
+    ## 1, 1
+    pushViewport(viewport(layout.pos.col=1, layout.pos.row=1))
+    semi()
+    pushViewport(viewport(height=.5))
+    cols <- outer(L, rev(C),
+                  function(x, y) colorFromHue(x, y, 180 - angle, sRGB))
+    grid.raster(cols, width=1, height=1)
+    nonNA <- which(!is.na(cols[quantile(L, .2),]))
+    grid.segments(min(nonNA)/ncol(cols), .8, 1, .8,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    grid.move.to(1, .8)
+    popViewport(2)
+    ## 1, 2
+    pushViewport(viewport(layout.pos.col=3, layout.pos.row=1),
+                 viewport(y=.5 - (.5 + r*sintheta - .65), angle=0))
+    circ()
+    cols <- outer(rev(u), v, function(x, y) colorFromUV(x, y, 80, sRGB))
+    slice(cols)
+    popViewport(2)
+    ## 1, 3
+    pushViewport(viewport(layout.pos.col=5, layout.pos.row=1))
+    semi(FALSE)
+    pushViewport(viewport(height=.5))
+    grid.line.to(0, .8, gp=gpar(col=rgb(1,1,1,dotalpha), lty="dotted"))
+    cols <- outer(L, C, function(x, y) colorFromHue(x, y, angle, sRGB))
+    grid.raster(cols, width=1, height=1)
+    nonNA <- which(!is.na(cols[quantile(L, .2),]))
+    grid.segments(max(nonNA)/ncol(cols), .8, 0, .8,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    popViewport(2)
 
-## 1, 1
-pushViewport(viewport(layout.pos.col=1, layout.pos.row=1))
-semi()
-pushViewport(viewport(height=.5))
-cols <- outer(L, rev(C), function(x, y) colorFromHue(x, y, 180 - angle))
-grid.raster(cols, width=1, height=1)
-nonNA <- which(!is.na(cols[quantile(L, .2),]))
-grid.segments(min(nonNA)/ncol(cols), .8, 1, .8,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-grid.move.to(1, .8)
-popViewport(2)
-## 1, 2
-pushViewport(viewport(layout.pos.col=3, layout.pos.row=1),
-             viewport(y=.5 - (.5 + r*sintheta - .65), angle=0))
-circ()
-cols <- outer(rev(u), v, function(x, y) colorFromUV(x, y, 80))
-slice(cols)
-popViewport(2)
-## 1, 3
-pushViewport(viewport(layout.pos.col=5, layout.pos.row=1))
-semi(FALSE)
-pushViewport(viewport(height=.5))
-grid.line.to(0, .8, gp=gpar(col=rgb(1,1,1,dotalpha), lty="dotted"))
-cols <- outer(L, C, function(x, y) colorFromHue(x, y, angle))
-grid.raster(cols, width=1, height=1)
-nonNA <- which(!is.na(cols[quantile(L, .2),]))
-grid.segments(max(nonNA)/ncol(cols), .8, 0, .8,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-popViewport(2)
+    ## 2, 1
+    pushViewport(viewport(layout.pos.col=1, layout.pos.row=3))
+    semi()
+    cols <- outer(L, rev(C), function(x, y) colorFromHue(x, y, 180, sRGB))
+    grid.raster(cols, width=1, height=.5)
+    nonNA <- which(!is.na(cols[median(L),]))
+    grid.segments(min(nonNA)/ncol(cols), .5, 1, .5,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    grid.move.to(1, .5)
+    popViewport()
+    ## 2, 2
+    pushViewport(viewport(layout.pos.col=3, layout.pos.row=3),
+                 viewport(angle=0))
+    circ()
+    cols <- outer(rev(u), v, function(x, y) colorFromUV(x, y, 50, sRGB))
+    grid.raster(cols)
+    nonNA <- which(!is.na(cols[median(1:length(v)),]))
+    grid.line.to(min(nonNA)/ncol(cols), .5, 
+                 gp=gpar(col=rgb(1, 1, 1, dotalpha), lty="dotted"))
+    grid.segments(min(nonNA)/ncol(cols), .5, .5, .5,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    grid.circle(.5, .5, unit(.5, "mm"),
+                gp=gpar(col=rgb(1, 1, 1, .2)))
+    grid.segments(max(nonNA)/ncol(cols), .5, .5, .5,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    grid.move.to(max(nonNA)/ncol(cols), .5)
+    popViewport(2)
+    ## 2, 3
+    pushViewport(viewport(layout.pos.col=5, layout.pos.row=3))
+    semi(FALSE)
+    cols <- outer(L, C, function(x, y) colorFromHue(x, y, 0, sRGB))
+    grid.raster(cols, width=1, height=.5)
+    nonNA <- which(!is.na(cols[median(L),]))
+    grid.line.to(0, .5, 
+                 gp=gpar(col=rgb(1, 1, 1, dotalpha), lty="dotted"))
+    grid.segments(max(nonNA)/ncol(cols), .5, 0, .5,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    popViewport()
 
-## 2, 1
-pushViewport(viewport(layout.pos.col=1, layout.pos.row=3))
-semi()
-cols <- outer(L, rev(C), function(x, y) colorFromHue(x, y, 180))
-grid.raster(cols, width=1, height=.5)
-nonNA <- which(!is.na(cols[median(L),]))
-grid.segments(min(nonNA)/ncol(cols), .5, 1, .5,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-grid.move.to(1, .5)
-popViewport()
-## 2, 2
-pushViewport(viewport(layout.pos.col=3, layout.pos.row=3),
-             viewport(angle=0))
-circ()
-cols <- outer(rev(u), v, function(x, y) colorFromUV(x, y, 50))
-grid.raster(cols)
-nonNA <- which(!is.na(cols[median(1:length(v)),]))
-grid.line.to(min(nonNA)/ncol(cols), .5, 
-             gp=gpar(col=rgb(1, 1, 1, dotalpha), lty="dotted"))
-grid.segments(min(nonNA)/ncol(cols), .5, .5, .5,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-grid.circle(.5, .5, unit(.5, "mm"),
-            gp=gpar(col=rgb(1, 1, 1, .2)))
-grid.segments(max(nonNA)/ncol(cols), .5, .5, .5,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-grid.move.to(max(nonNA)/ncol(cols), .5)
-popViewport(2)
-## 2, 3
-pushViewport(viewport(layout.pos.col=5, layout.pos.row=3))
-semi(FALSE)
-cols <- outer(L, C, function(x, y) colorFromHue(x, y, 0))
-grid.raster(cols, width=1, height=.5)
-nonNA <- which(!is.na(cols[median(L),]))
-grid.line.to(0, .5, 
-             gp=gpar(col=rgb(1, 1, 1, dotalpha), lty="dotted"))
-grid.segments(max(nonNA)/ncol(cols), .5, 0, .5,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-popViewport()
+    ## 3, 1
+    pushViewport(viewport(layout.pos.col=1, layout.pos.row=5))
+    semi()
+    pushViewport(viewport(height=.5))
+    cols <- outer(L, rev(C),
+                  function(x, y) colorFromHue(x, y, 180 + angle, sRGB))
+    grid.raster(cols, width=1, height=1)
+    nonNA <- which(!is.na(cols[quantile(L, .8),]))
+    grid.segments(min(nonNA)/ncol(cols), .2, 1, .2,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    grid.move.to(1, .2)
+    popViewport(2)
+    ## 3, 2
+    pushViewport(viewport(layout.pos.col=3, layout.pos.row=5),
+                 viewport(y=.5 + (.5 + r*sintheta - .65), angle=0))
+    circ()
+    cols <- outer(rev(u), v, function(x, y) colorFromUV(x, y, 20, sRGB))
+    slice(cols, up=FALSE)
+    popViewport(2)
+    ## 3, 3 
+    pushViewport(viewport(layout.pos.col=5, layout.pos.row=5))
+    semi(FALSE)
+    pushViewport(viewport(height=.5))
+    grid.line.to(0, .2, gp=gpar(col=rgb(1,1,1,dotalpha), lty="dotted"))
+    cols <- outer(L, C, function(x, y) colorFromHue(x, y, 360 - angle, sRGB))
+    grid.raster(cols, width=1, height=1)
+    nonNA <- which(!is.na(cols[quantile(L, .8),]))
+    grid.segments(max(nonNA)/ncol(cols), .2, 0, .2,
+                  gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
+    popViewport(2)
+}
 
-## 3, 1
-pushViewport(viewport(layout.pos.col=1, layout.pos.row=5))
-semi()
-pushViewport(viewport(height=.5))
-cols <- outer(L, rev(C), function(x, y) colorFromHue(x, y, 180 + angle))
-grid.raster(cols, width=1, height=1)
-nonNA <- which(!is.na(cols[quantile(L, .8),]))
-grid.segments(min(nonNA)/ncol(cols), .2, 1, .2,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-grid.move.to(1, .2)
-popViewport(2)
-## 3, 2
-pushViewport(viewport(layout.pos.col=3, layout.pos.row=5),
-             viewport(y=.5 + (.5 + r*sintheta - .65), angle=0))
-circ()
-cols <- outer(rev(u), v, function(x, y) colorFromUV(x, y, 20))
-slice(cols, up=FALSE)
-grid.circle(.5, .5, unit(.5, "mm"),
-            gp=gpar(col=rgb(1, 1, 1, .2)))
-popViewport(2)
-## 3, 3 
-pushViewport(viewport(layout.pos.col=5, layout.pos.row=5))
-semi(FALSE)
-pushViewport(viewport(height=.5))
-grid.line.to(0, .2, gp=gpar(col=rgb(1,1,1,dotalpha), lty="dotted"))
-cols <- outer(L, C, function(x, y) colorFromHue(x, y, 360 - angle))
-grid.raster(cols, width=1, height=1)
-nonNA <- which(!is.na(cols[quantile(L, .8),]))
-grid.segments(max(nonNA)/ncol(cols), .2, 0, .2,
-              gp=gpar(col=rgb(1, 1, 1, .2), lwd=2))
-popViewport(2)
+png("hcl-srgb.png", width=600, height=800)
+hclDiagram()
+dev.off()
 
+pause <- function() {
+    png("hcl.png", width=600, height=800)
+    hclDiagram(sRGB=FALSE)
+    dev.off()
+}
